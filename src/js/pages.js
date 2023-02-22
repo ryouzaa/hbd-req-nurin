dc7bdf06be7e47b8d75efded67cd3fb33f28ecce
Faisal Alfainun <57319059+ryouzaa@users.noreply.github.com>
2023-02-22 14:11:39 +0700

    Initial commit

---
+++ src/js/pages.js
@@ -0,0 +1,22 @@
+export const soon = {
+  title: "Datang Kembali Nanti...",
+  body: `<br /><br /><br /><br /><br /><br /><br /><br />
+    <h1 style='color:#000;margin-top:15%;'>Hai, Kamu datang terlalu awal</h1>
+    <br /><br /><br />
+    <p style='color:#000;'>
+        Aku tau halaman ini sangat menarik bagimu, khususnya untuk hari spesialmu tapi.<br />
+        Kamu harus lebih bersabar sampai waktunya tiba, benar ?
+    </p>`,
+};
+
+export const late = {
+  title: "Sampai Jumpa Kembali...",
+  body: `<br /><br /><br /><br /><br /><br /><br /><br />
+    <h1 style='color:#000;margin-top:15%;'>Pesta Ulang Tahun Telah Selesai</h1>
+    <br /><br /><br />
+    <p style='color:#000;'>
+        Ya, kado ku untuk mu sangat sederhana, murah, dan aneh ? &#128534<br>
+        T-tapi. Itu hanya untukmu. &#128150
+    </p>
+`,
+};
